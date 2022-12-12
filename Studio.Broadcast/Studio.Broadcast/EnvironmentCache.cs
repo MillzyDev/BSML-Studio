6884722fad514af60d5923254f2fd4976ab5fb03
Millzy <millzyyt08@gmail.com>
2022-12-12 21:15:15 +0000

    yes... "cache"

---
+++ Studio.Broadcast/Studio.Broadcast/EnvironmentCache.cs
@@ -0,0 +1,81 @@
+﻿namespace Studio.Broadcast
+{
+    public delegate void OnRefresh();
+    public delegate void OnReload();
+
+    public class EnvironmentCache : IDisposable
+    {
+        private static readonly Lazy<EnvironmentCache> _lazy = new(() => new EnvironmentCache());
+
+        public event OnRefresh? OnRefresh;
+        public event OnReload? OnReload;
+
+        private readonly Dictionary<string, string> cacheData = new();
+        private readonly Task refreshRoutine;
+        private bool refresh;
+
+        private Environment environment;
+
+        private EnvironmentCache()
+        {
+            environment = Environment.Instance;
+
+            Refresh();
+
+            refresh = true;
+            refreshRoutine = Task.Run(AutoRefreshRoutine);
+        }
+
+        public static EnvironmentCache Instance => _lazy.Value;
+
+        public void Refresh()
+        {
+            OnRefresh?.Invoke();
+
+            string[] files = environment.GetFiles();
+
+            bool mismatch = false;
+            foreach (string file in files)
+            {
+                try
+                {
+                    string content = File.ReadAllText(file);
+                    mismatch = cacheData[file] != content;
+                }
+                catch
+                {
+                    mismatch = true;
+                }
+            }
+
+            if (files.Length != cacheData.Count || mismatch)
+            {
+                // Reload
+                foreach (string file in files)
+                {
+                    cacheData[file] = File.ReadAllText(file);
+                }
+
+                OnReload?.Invoke();
+            }
+        }
+
+        public void Cease()
+        {
+            refresh = false;
+        }
+
+        private void AutoRefreshRoutine()
+        {
+            while (refresh)
+            {
+
+            }
+        }
+
+        public void Dispose()
+        {
+            Cease();
+        }
+    }
+}
