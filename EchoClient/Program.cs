487f4983c3fba723df00d0570e0fa7a29bca8ea2
Millzy <millzyyt08@gmail.com>
2022-12-22 18:11:05 +0000

    test client

---
+++ EchoClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+
+namespace EchoServer
+{
+    public class Program
+    {
+        public static readonly uint port = 62098;
+
+        public static void Main(string[] args)
+        {
+            using (var client = new RequestSocket())
+            {
+                client.Connect($"tcp://localhost:{port}");
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("Sending Hello");
+                    client.SendFrame("Hello");
+                    var message = client.ReceiveFrameString();
+                    Console.WriteLine("Received {0}", message);
+                }
+            }
+        }
+    }
+}
