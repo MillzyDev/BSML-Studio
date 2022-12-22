b22e8b1e4e154dcd010c568309d20a0be6071905
Millzy <millzyyt08@gmail.com>
2022-12-22 18:11:20 +0000

    test server

--- EchoServer/Program.cs
+++ EchoServer/Program.cs
@@ -1,61 +1,26 @@
-﻿using System.Net;
-using System.Net.Sockets;
-using System.Text;
+﻿using NetMQ;
+using NetMQ.Sockets;
 
 namespace EchoServer
 {
     public class Program
     {
-        public static readonly int port = 11000;
+        public static readonly uint port = 62098;
 
         public static void Main(string[] args)
         {
-            IPHostEntry ipHost = Dns.GetHostEntry("localhost");
-            IPAddress ipAddress = ipHost.AddressList[0];
-            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port);
-
-            Console.WriteLine("Listening on {0}:{1}", ipAddress.ToString(), port);
-
-            try
+            using (var server = new ResponseSocket())
             {
-                Socket listener = new(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                server.Bind($"tcp://localhost:{port}");
 
-                listener.Bind(localEndPoint);
-                listener.Listen(10);
-
-                Console.WriteLine("Waiting for a connection...");
-                Socket handler = listener.Accept();
-
-                string data = null;
-                byte[] bytes = null;
-                
                 while (true)
                 {
-                    bytes = new byte[1024];
-                    int bytsRec = handler.Receive(bytes);
-                    data += Encoding.ASCII.GetString(bytes, 0, bytsRec);
-
-                    if (data.IndexOf("<EOF>") > -1)
-                    {
-                        break;
-                    }
+                    var message = server.ReceiveFrameString();
+                    Console.WriteLine(message);
+                    Thread.Sleep(100);
+                    server.SendFrame(message);
                 }
-
-                Console.WriteLine("Text recieved : {0}", data);
-
-                byte[] msg = Encoding.ASCII.GetBytes(data);
-                handler.Send(msg);
-                handler.Shutdown(SocketShutdown.Both);
-                handler.Close();
             }
-
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
-
-            Console.WriteLine("\n Press any key to continue...");
-            Console.ReadKey();
         }
     }
 }
