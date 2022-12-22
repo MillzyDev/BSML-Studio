6cc392e818ac1996272adfa30b4baed4b07ac7f7
Millzy <millzyyt08@gmail.com>
2022-12-22 18:10:41 +0000

    use FileSystemWatcher

--- Studio.Broadcast/Studio.Broadcast/EnvironmentCache.cs
+++ Studio.Broadcast/Studio.Broadcast/EnvironmentCache.cs
@@ -1,17 +1,18 @@
 ﻿namespace Studio.Broadcast
 {
-    public delegate void OnRefresh();
-    public delegate void OnReload();
+    /// <summary>
+    /// Runs when the <see cref="EnvironmentCache"/> begins to check files for edits.
+    /// </summary>
+    /// <param name="files">Array of file paths that are checked.</param>
+    public delegate void OnRefresh(string[] files);
 
     public class EnvironmentCache : IDisposable
     {
         private static readonly Lazy<EnvironmentCache> _lazy = new(() => new EnvironmentCache());
 
         public event OnRefresh? OnRefresh;
-        public event OnReload? OnReload;
 
         private readonly Dictionary<string, string> cacheData = new();
-        private bool refresh;
 
         private Environment environment;
 
@@ -19,62 +20,51 @@
         {
             environment = Environment.Instance;
 
-            Refresh();
+            using var watcher = new FileSystemWatcher(environment.EnvironmentPath);
+            watcher.NotifyFilter = // main things we want to trigger the cache to update.
+                NotifyFilters.FileName
+                | NotifyFilters.Size
+                | NotifyFilters.LastWrite
+                | NotifyFilters.Attributes;
 
-            refresh = true;
-            Task.Run(AutoRefreshRoutine);
+            watcher.Changed += OnChange;
+
+            watcher.Filter = "*.bsml"; // only bsml files
+            watcher.IncludeSubdirectories = false;
+            watcher.EnableRaisingEvents = true; // enable our OnChange event to be invoked.
+
+            Refresh();
+        }
+
+        /// <summary>
+        /// NOTE: This will only update files already registered within the cache.
+        /// New files can be registered in <see cref="Refresh"/>
+        /// </summary>
+        private void OnChange(object sender, FileSystemEventArgs e)
+        {
+            
+        }
+
+        /// <summary>
+        /// Reloads all files in the cache
+        /// </summary>
+        public void Refresh()
+        {
+            string[] files = Environment.Instance.GetFiles();
+
+            foreach (var file in files)
+            {
+                cacheData[file] = File.ReadAllText(file);
+            }
+
+            OnRefresh?.Invoke(files);
         }
 
         public static EnvironmentCache Instance => _lazy.Value;
 
-        public void Refresh()
-        {
-            OnRefresh?.Invoke();
-
-            string[] files = environment.GetFiles();
-
-            bool mismatch = false;
-            foreach (string file in files)
-            {
-                try
-                {
-                    string content = File.ReadAllText(file);
-                    mismatch = cacheData[file] != content;
-                }
-                catch
-                {
-                    mismatch = true;
-                }
-            }
-
-            if (files.Length != cacheData.Count || mismatch)
-            {
-                // Reload
-                foreach (string file in files)
-                {
-                    cacheData[file] = File.ReadAllText(file);
-                }
-
-                OnReload?.Invoke();
-            }
-        }
-
-        public void Cease()
-        {
-            refresh = false;
-        }
-
-        private void AutoRefreshRoutine()
-        {
-            do
-            {
-                Refresh();
-            } while (refresh);
-        }
-
         public void Dispose()
         {
-            Cease();
+            //Cease();
         }
     }
 }
