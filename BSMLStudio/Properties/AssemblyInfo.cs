20fa7dbff8e74f1bb59ade2f98269ca9d049e178
Millzy <millzyyt08@gmail.com>
2022-12-24 17:53:41 +0000

    migrate to AssemblyInfo.cs

---
+++ BSMLStudio/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: AssemblyCompany("BSMLStudio")]
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyProduct("BSMLStudio")]
+[assembly: AssemblyTitle("BSMLStudio")]
+[assembly: AssemblyVersion("1.0.0.0")]
