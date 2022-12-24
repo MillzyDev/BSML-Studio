6b549dd59671c67f8d6569ddf7bb0ec9307ff643
Millzy <millzyyt08@gmail.com>
2022-12-24 17:28:25 +0000

    placeholder

---
+++ BSMLStudio/Commands/ListCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSMLStudio.Commands
+{
+    public class ListCommand : ICommand
+    {
+        public string Name { get; set; } = "list";
+        public string Description { get; set; } = "Lists all files currently in the BSMLStudio Environment.";
+        public string Usage { get; set; } = "";
+        public uint RequiredArgsCount { get; set; } = 0;
+
+        public bool Execute(string[] args)
+        {
+            Console.WriteLine("Not yet finished!");
+
+            return true;
+        }
+    }
+}
