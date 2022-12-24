50431bc467c3b4be36280236bc1b847b16a6f602
Millzy <millzyyt08@gmail.com>
2022-12-24 17:28:11 +0000

    commands base

---
+++ BSMLStudio/ICommand.cs
@@ -0,0 +1,12 @@
+﻿namespace BSMLStudio
+{
+    public interface ICommand
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Usage { get; set; }
+        public uint RequiredArgsCount { get; set; }
+
+        public bool Execute(string[] args);
+    }
+}
