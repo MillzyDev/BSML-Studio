e953ff47cf1dc298f4ab8257c06558cd78e934ff
Millzy <millzyyt08@gmail.com>
2022-12-24 14:54:57 +0000

    environment paths

---
+++ BSMLStudio/StudioEnvironment.cs
@@ -0,0 +1,8 @@
+﻿namespace BSMLStudio
+{
+    internal class StudioEnvironment
+    {
+        public static string Environment { get => Directory.GetCurrentDirectory(); }
+        public static string Logs { get => Path.Combine(Environment, "Logs"); }
+    }
+}
