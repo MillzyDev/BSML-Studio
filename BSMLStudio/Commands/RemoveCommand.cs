6acb1341c26c8bef25fb23561af0a53f40087342
Millzy <millzyyt08@gmail.com>
2023-01-02 11:02:17 +0000

    Remove Command

---
+++ BSMLStudio/Commands/RemoveCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace BSMLStudio.Commands
+{
+    public class RemoveCommand : ICommand
+    {
+        public string Name { get; set; } = "remove";
+        public string Description { get; set; } = "Removes a file from the environment.";
+        public string Usage { get; set; } = "<filepath>";
+        public uint RequiredArgsCount { get; set; } = 1;
+
+        public bool Execute(string[] args)
+        {
+            Environment.Instance.RemoveFromEnvironment(args[0]);
+            Console.WriteLine("Successfully removed file from the environment!");
+            return true;
+        }
+    }
+}
