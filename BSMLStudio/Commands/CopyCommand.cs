379f580821ad1271a5bd0e45411bf97bdb72b716
Millzy <millzyyt08@gmail.com>
2023-01-02 11:01:46 +0000

    Copy Command

---
+++ BSMLStudio/Commands/CopyCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace BSMLStudio.Commands
+{
+    public class CopyCommand : ICommand
+    {
+        public string Name { get; set; } = "copy";
+        public string Description { get; set; } = "Copies a file to the environment.";
+        public string Usage { get; set; } = "<filepath>";
+        public uint RequiredArgsCount { get; set; } = 1;
+
+        public bool Execute(string[] args)
+        {
+            Environment.Instance.CopyToEnvironment(args[0]);
+            Console.WriteLine("Successfully copied file to the environment!");
+            return true;
+        }
+    }
+}
