1f8602bd0c4f21893b63a86d08a1dd1449b21b81
Millzy <millzyyt08@gmail.com>
2023-01-02 11:02:09 +0000

    Location Command

---
+++ BSMLStudio/Commands/LocationCommand.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+namespace BSMLStudio.Commands
+{
+    public class LocationCommand : ICommand
+    {
+        public string Name { get; set; } = "location";
+        public string Description { get; set; } = "Prints the environment path to the console or opens the environment folder.";
+        public string Usage { get; set; } = "[open]";
+        public uint RequiredArgsCount { get; set; } = 0;
+
+        public bool Execute(string[] args)
+        {
+            if (args.Length > RequiredArgsCount && args[0].ToLower() == "open")
+            {
+                Process.Start("explorer.exe", Environment.Instance.EnvironmentPath);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine(Environment.Instance.EnvironmentPath);
+                return true;
+            }
+        }
+    }
+}
